4c002832333a2dea52501ec01286e69c3fa04bf4
arkodaz <110835380+arkodaz@users.noreply.github.com>
2023-02-05 04:08:53 -0800

    Update runtime.js

--- interactive/runtime.js
+++ interactive/runtime.js
@@ -1,3 +1,6 @@
+var pist = document.getElementsByClassName("text-pis")[0]
+var ortat = document.getElementsByClassName("text-orta")[0]
+var elat = document.getElementsByClassName("text-ela")[0]
 const store = new SteinStore(
     "https://api.steinhq.com/v1/storages/63d69b10eced9b09e9bbb99f"
   );
@@ -6,4 +9,9 @@ const store = new SteinStore(
     var pis = data[0].pis;
     var orta = data[0].orta;
     var ela = data[0].ela;
+
+    pist.setAttribute("value" , '"Pis" rəyi verənlər:'+ pis)
+    ortat.setAttribute("value" , '"Orta" rəyi verənlər:'+ orta)
+    elat.setAttribute("value" , '"Əla" rəyi verənlər:'+ ela)
+
   });
